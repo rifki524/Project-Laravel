acf718817a3d2693b9b7284b3c9f53687e188546
rifkisutrisna <rifki.supriatna.mr@gmail.com>
2018-11-12 16:48:31 +0700

    push

---
+++ app/Models/text.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class text extends Model
+{
+    protected $table = 'text';
+}
---
+++ database/migrations/2018_10_13_075903_create_quotes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('slug', 50);
+            $table->text('subject', 255);
+
+            $table->integer('user_id')->unsigned(); //buat disambungin
+            //relasi user id ke id di table users
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
